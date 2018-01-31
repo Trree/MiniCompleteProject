6afb43451dccee7a61ad440e4a696dc0570dc4a7
trree <wufl@koal.com>
2018-01-31 10:22:43 +0800

    fix clang-tidy

--- cmake/clang-cxx-dev-tools.cmake
+++ cmake/clang-cxx-dev-tools.cmake
@@ -2,7 +2,7 @@
 
 # Additional targets to perform clang-format/clang-tidy
 # Get all project files
-file(GLOB_RECURSE
+file(GLOB
   ALL_CXX_SOURCE_FILES
   *.[chi]pp *.[chi]xx *.cc *.hh *.ii *.[CHI]
   )
@@ -20,7 +20,6 @@ if(CLANG_FORMAT)
 endif()
 
 
-set(clang_tidy_config "{Checks: '*,-misc-macro-parentheses,-misc-definitions-in-headers,-misc-unused-parameters,-llvm-header-guard,-llvm-include-order,-google-readability-braces-around-statements,-google-runtime-references,-google-readability-todo,-google-build-using-namespace,-google-explicit-constructor,-cert-err58-cpp,-cppcoreguidelines-pro-type-vararg,-cppcoreguidelines-pro-bounds-pointer-arithmetic,-cppcoreguidelines-pro-bounds-array-to-pointer-decay,-cppcoreguidelines-special-member-functions,-cppcoreguidelines-pro-type-reinterpret-cast,-cppcoreguidelines-pro-bounds-constant-array-index,-cppcoreguidelines-pro-type-member-init,-cppcoreguidelines-pro-type-union-access,-clang-analyzer-security.insecureAPI.strcpy,-modernize-use-nullptr,-modernize-use-equals-default,-modernize-loop-convert,-modernize-use-auto,-readability-braces-around-statements,-readability-named-parameter,-readability-else-after-return,-readability-redundant-declaration,-readability-implicit-bool-cast'}")
 
 
 # Adding clang-tidy target if executable is found
@@ -29,6 +28,6 @@ if(CLANG_TIDY)
   message(STATUS "${clang_tidy_config}")
   add_custom_target(
     clang-tidy
-    COMMAND clang-tidy ${ALL_CXX_SOURCE_FILES}  -checks='*,-misc-macro-parentheses,-misc-definitions-in-headers,-misc-unused-parameters,-llvm-header-guard,-llvm-include-order,-google-readability-braces-around-statements,-google-runtime-references,-google-readability-todo,-google-build-using-namespace,-google-explicit-constructor,-cert-err58-cpp,-cppcoreguidelines-pro-type-vararg,-cppcoreguidelines-pro-bounds-pointer-arithmetic,-cppcoreguidelines-pro-bounds-array-to-pointer-decay,-cppcoreguidelines-special-member-functions,-cppcoreguidelines-pro-type-reinterpret-cast,-cppcoreguidelines-pro-bounds-constant-array-index,-cppcoreguidelines-pro-type-member-init,-cppcoreguidelines-pro-type-union-access,-clang-analyzer-security.insecureAPI.strcpy,-modernize-use-nullptr,-modernize-use-equals-default,-modernize-loop-convert,-modernize-use-auto,-readability-braces-around-statements,-readability-named-parameter,-readability-else-after-return,-readability-redundant-declaration,-readability-implicit-bool-cast'
+    COMMAND clang-tidy ${ALL_CXX_SOURCE_FILES} -header-filter='.*'  -checks='*,-misc-macro-parentheses,-misc-definitions-in-headers,-misc-unused-parameters,-llvm-header-guard,-llvm-include-order,-google-readability-braces-around-statements,-google-runtime-references,-google-readability-todo,-google-build-using-namespace,-google-explicit-constructor,-cert-err58-cpp,-cppcoreguidelines-pro-type-vararg,-cppcoreguidelines-pro-bounds-pointer-arithmetic,-cppcoreguidelines-pro-bounds-array-to-pointer-decay,-cppcoreguidelines-special-member-functions,-cppcoreguidelines-pro-type-reinterpret-cast,-cppcoreguidelines-pro-bounds-constant-array-index,-cppcoreguidelines-pro-type-member-init,-cppcoreguidelines-pro-type-union-access,-clang-analyzer-security.insecureAPI.strcpy,-modernize-use-nullptr,-modernize-use-equals-default,-modernize-loop-convert,-modernize-use-auto,-readability-braces-around-statements,-readability-named-parameter,-readability-else-after-return,-readability-redundant-declaration,-readability-implicit-bool-cast' -- -std=c++11
   )
 endif()
--- test/hello_world.cpp
+++ test/hello_world.cpp
@@ -1,16 +1,20 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
-int fact(int n);
+long long fib(int n);
 
-int fact(int n) {
-    return n <= 1 ? n : fact(n - 1) * n;
+// It would be overflow_error
+long long fib(int n) {
+  const double sqrt5 = std::sqrt(5);
+  const double phi = (1 + sqrt5) / 2;
+  return static_cast<long long>(std::pow(phi, n+1) / sqrt5 + 0.5);
 }
 
-TEST_CASE("testing the factorial function") {
-    CHECK(fact(0) == 0);
-    CHECK(fact(1) == 1);
-    CHECK(fact(2) == 2);
-    CHECK(fact(3) == 6);
-    CHECK(fact(10) == 3628800);
+TEST_CASE("testing the fibonacci function") {
+    CHECK(fib(0) == 1);
+    CHECK(fib(1) == 1);
+    CHECK(fib(2) == 2);
+    CHECK(fib(3) == 3);
+    CHECK(fib(10) == 89);
+    CHECK(fib(50) == 20365011074);
 }
